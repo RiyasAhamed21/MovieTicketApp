df029826f95e930cc8c1285de8d3d1e313a46717
Riyas Ahamed U <Riyas21052@gmail.com>
2021-05-04 12:30:46 +0530

    Added Comments

--- src/main/java/com/riyasahamed/UserManager.java
+++ src/main/java/com/riyasahamed/UserManager.java
@@ -6,10 +6,16 @@ public class UserManager {
 
 	static HashMap<Long, String> users = new HashMap<Long, String>();
 
+	/*
+	 * This Method add User
+	 */
 	public static void addUser(Long mobileNo, String password) {
 		users.put(mobileNo, password);
 	}
-
+	/*
+	 * This Method Remove User
+	 */
+	
 	public static void removeUser(Long mobileNo) {
 		if (users.containsKey(mobileNo)) {
 			users.remove(mobileNo);
